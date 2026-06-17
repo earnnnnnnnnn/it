8d051afb1cbd793d8a111b5894e299ede2406fdd
eannnnnnnnn <s6606021631017@email.kmutnb.ac.th>
2026-06-17 13:56:01 +0700

    17/6/2569 แก้database

--- it_inventory.sql
+++ it_inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 17, 2026 at 07:08 AM
+-- Generation Time: Jun 17, 2026 at 08:55 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
